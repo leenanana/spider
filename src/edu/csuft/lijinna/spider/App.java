9707550964335a837cc9b7206cdebfaddff5f9c3
leenanana <45841465+leenanana@users.noreply.github.com>
2018-12-16 17:21:46 +0800

    解析影片信息

--- src/edu/csuft/lijinna/spider/App.java
+++ src/edu/csuft/lijinna/spider/App.java
@@ -1,14 +1,59 @@
 package edu.csuft.lijinna.spider;
 
+import java.io.IOException;
+import java.util.ArrayList;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class App {
+   
+	//	alt+/
 	
-	
-
+    //ctrl+s
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+   //		目标URL
+		String url="https://movie.douban.com/top250";
+     //		使用JSOUP抓取文档
+		try {
+			Document doc= Jsoup.connect(url).get();
+			
+			Elements es =doc.select(".grid_view .item");
+			System.out.println(es.size());
+			
+//			创建一个影片的列表
+			ArrayList<film> list=new ArrayList<>();
+			
+			for(Element e :es) {
+//				.代表的是class
+//				  每一部影片
+				film f=new film();
+				
+				Element t =e.select(".title").first();
+				String num=e.select(".star span").last().text();
+				System.out.println(t.text()+","+num);
+				System.out.println(t.text());
 
+//				f.id
+//				f.title
+				list.add(f);
+				
+			}
+			
+//			String title=doc.title();
+//			String data=doc.data();
+//			
+//			System.out.println(title);
+//			System.out.println(data);
+		    	
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
---
+++ src/edu/csuft/lijinna/spider/film.java
@@ -0,0 +1,34 @@
+package edu.csuft.lijinna.spider;
+
+public class film {
+/**
+ 影片名称
+ */
+	String title;
+/**
+ * 相关信息
+ */
+	String info;
+/**
+ * 	评分
+ */
+	double rating;
+/**
+ * 评分人数
+ */
+	int num;
+	/**
+	 * 影片的排名
+	 */
+	int id;
+	/**
+	 * 海报
+	 */
+	String poster;
+	/**
+	 * 短评
+	 */
+	String quote;
+	
+	
+}
